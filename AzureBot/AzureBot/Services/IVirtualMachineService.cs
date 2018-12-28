8b2b493e6b7c80dc243f4a7a5c839e8c7b8f4859
peter franceschini <peter.franceschini@verndale.com>
2018-12-28 11:31:57 -0500

    Implemented VirtualMachineService

---
+++ AzureBot/AzureBot/Services/IVirtualMachineService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureBot.Services
+{
+    public interface IVirtualMachineService
+    {
+        void Start(string machineName);
+        void Stop(string machineName);
+        bool IsRunning(string machineName);
+    }
+}
---
+++ AzureBot/AzureBot/Services/VirtualMachineService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.Management.Compute.Fluent;
+using Microsoft.Azure.Management.Fluent;
+using System.Linq;
+
+namespace AzureBot.Services
+{
+    public class VirtualMachineService : IVirtualMachineService
+    {
+        private IAzure _Azure { get; set; }
+
+        public VirtualMachineService()
+        {
+            _Azure = Azure.Authenticate("Cred_file_path").WithDefaultSubscription();
+        }
+
+        public void Start(string machineName)
+        {
+            var vm = GetVirtualMachineByName(machineName);
+            vm.Start();
+        }
+
+        public void Stop(string machineName)
+        {
+            var vm = GetVirtualMachineByName(machineName);
+            vm.PowerOff();
+        }
+
+        public bool IsRunning(string machineName)
+        {
+            var machine = GetVirtualMachineByName(machineName);
+            return machine.PowerState == PowerState.Running;
+        }
+
+        private IVirtualMachine GetVirtualMachineByName(string machineName)
+        {
+            var virtualMachines = _Azure.VirtualMachines.List();
+            return virtualMachines.FirstOrDefault(v => v.Name.ToLower() == machineName.ToLower());
+        }
+    }
+}
