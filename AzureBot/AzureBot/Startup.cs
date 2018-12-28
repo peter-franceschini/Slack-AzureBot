07262c5bf19338a01b0b0d9446c79d5636f4c0b0
peter franceschini <peter.franceschini@verndale.com>
2018-12-28 11:35:17 -0500

    Added DI support for IVirtualMachineService

--- AzureBot/AzureBot/Startup.cs
+++ AzureBot/AzureBot/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using AzureBot.Services;
 
 namespace AzureBot
 {
@@ -25,6 +26,7 @@ namespace AzureBot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IVirtualMachineService, VirtualMachineService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
