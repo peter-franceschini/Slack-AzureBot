895e0e34f049b5527daae7059f3bf0a5b8318a2d
peter franceschini <peter.franceschini@verndale.com>
2018-12-28 22:11:21 -0500

    Added VirtualMachineCommand model

---
+++ AzureBot/AzureBot/Models/VirtualMachineCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureBot.Models
+{
+    public class VirtualMachineCommand
+    {
+        public string Action { get; set; }
+        public string Target { get; set; }
+    }
+}
