6701bbd933ba5a220d1f1e929ff9470606677133
peter franceschini <peter.franceschini@verndale.com>
2018-12-28 22:11:01 -0500

    Added CommandParseService to parse slack command text into data model in a consistent way

---
+++ AzureBot/AzureBot/Services/ICommandParseService.cs
@@ -0,0 +1,13 @@
+﻿using AzureBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureBot.Services
+{
+    public interface ICommandParseService
+    {
+        VirtualMachineCommand ParseCommand(string text);
+    }
+}
---
+++ AzureBot/AzureBot/Services/VirtualMachineCommandParseService.cs
@@ -0,0 +1,32 @@
+﻿using AzureBot.Models;
+
+namespace AzureBot.Services
+{
+    public class VirtualMachineCommandParseService : ICommandParseService
+    {
+        public VirtualMachineCommand ParseCommand(string text)
+        {
+            text = text.ToLower();
+            var parts = text.Split(" ");
+
+            if (parts.Length == 2)
+            {
+                return new VirtualMachineCommand()
+                {
+                    Action = parts[0],
+                    Target = parts[1]
+                };
+            }
+            else if (parts.Length == 3)
+            {
+                return new VirtualMachineCommand()
+                {
+                    Action = $"{parts[0]} {parts[1]}",
+                    Target = parts[2]
+                };
+            }
+
+            return new VirtualMachineCommand();
+        }
+    }
+}
